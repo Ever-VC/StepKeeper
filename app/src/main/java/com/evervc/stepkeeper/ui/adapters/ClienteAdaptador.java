68df888633c1d72f033f20407ac3d5297ece0123
Ever-VC <ever.vasquezc27@gmail.com>
2025-04-27 07:46:35 -0600

    :sparkles: feat: CRUD de clientes completo

--- app/src/main/java/com/evervc/stepkeeper/ui/adapters/ClienteAdaptador.java
+++ app/src/main/java/com/evervc/stepkeeper/ui/adapters/ClienteAdaptador.java
@@ -1,6 +1,9 @@
 package com.evervc.stepkeeper.ui.adapters;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,24 +11,31 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.evervc.stepkeeper.R;
+import com.evervc.stepkeeper.database.ZapateriaDatabase;
 import com.evervc.stepkeeper.models.Cliente;
+import com.evervc.stepkeeper.ui.ClientesActivity;
 import com.evervc.stepkeeper.ui.functions.ClienteListener;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ClienteAdaptador extends RecyclerView.Adapter<ClienteAdaptador.ClienteViewHolder> {
     private List<Cliente> lstClientes;
     private Context context;
     private ClienteListener listener;
+    private ActivityResultLauncher<Intent> activityLauncherClientes;
 
-    public ClienteAdaptador(List<Cliente> lstClientes, Context context, ClienteListener listener) {
+    public ClienteAdaptador(List<Cliente> lstClientes, Context context, ClienteListener listener, ActivityResultLauncher<Intent> activityLauncherClientes) {
         this.lstClientes = lstClientes;
         this.context = context;
         this.listener = listener;
+        this.activityLauncherClientes = activityLauncherClientes;
     }
 
     @NonNull
@@ -45,14 +55,32 @@ public class ClienteAdaptador extends RecyclerView.Adapter<ClienteAdaptador.Clie
         holder.btnEditarCliente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                // Abre el Activity de ClientesActivity y envía el id del cliente a editar
+                Intent editarCliente = new Intent(context, ClientesActivity.class);
+                editarCliente.putExtra("idClienteAEditar", cliente.getId());
+                activityLauncherClientes.launch(editarCliente);
             }
         });
 
         holder.btnEliminarCliente.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                listener.actualizarListaClientes();
+                AlertDialog.Builder mensajeDeConfirmacion = new AlertDialog.Builder(context);
+                mensajeDeConfirmacion.setTitle("¿Está seguro que desea eliminar el registro?");
+                mensajeDeConfirmacion.setMessage("Esta acción no puede revertirse, por lo tanto los cambios serán permanentes.");
+                mensajeDeConfirmacion.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        ExecutorService executorService = Executors.newSingleThreadExecutor();
+                        executorService.execute(() -> {
+                            ZapateriaDatabase db = ZapateriaDatabase.getInstancia(context);
+                            int resultado = db.clienteDao().eliminarCliente(cliente);
+                            if (resultado > 0) {
+                                listener.actualizarListaClientes();
+                            }
+                        });
+                    }
+                }).setNegativeButton("No", null).show();
             }
         });
     }
--- app/src/main/java/com/evervc/stepkeeper/ui/fragments/ClienteFragment.java
+++ app/src/main/java/com/evervc/stepkeeper/ui/fragments/ClienteFragment.java
@@ -1,16 +1,39 @@
 package com.evervc.stepkeeper.ui.fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.evervc.stepkeeper.R;
+import com.evervc.stepkeeper.database.ZapateriaDatabase;
+import com.evervc.stepkeeper.models.Cliente;
+import com.evervc.stepkeeper.ui.ClientesActivity;
+import com.evervc.stepkeeper.ui.adapters.ClienteAdaptador;
+
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class ClienteFragment extends Fragment {
+    private RecyclerView rcvClientes;
+    private Button btnNuevoCliente;
+    private TextView tvMensajeInfo;
+    private ActivityResultLauncher<Intent> activityLauncherClientes;
+
 
     public ClienteFragment() {
         // Required empty public constructor
@@ -30,6 +53,61 @@ public class ClienteFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_cliente, container, false);
+
+        asociarElementosXml(view);
+
+        actualizarListaClientes();
+
+        activityLauncherClientes = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        // Actualizar el recyclervire
+                        actualizarListaClientes();
+                    }
+                }
+        );
+
+        btnNuevoCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nuevoCiente = new Intent(getContext(), ClientesActivity.class);
+                activityLauncherClientes.launch(nuevoCiente);
+            }
+        });
+
         return view;
+    }
+
+    private void asociarElementosXml(View view) {
+        rcvClientes = view.findViewById(R.id.rcvClientes);
+        btnNuevoCliente = view.findViewById(R.id.btnNuevoCliente);
+        tvMensajeInfo = view.findViewById(R.id.tvMensajeInfo);
+    }
+
+    private void actualizarListaClientes() {
+        // Extrae todos los clientes de la base de datos y los muestra en el recyclerview
+        ZapateriaDatabase db = ZapateriaDatabase.getInstancia(getContext());
+
+        Handler handler = new Handler(Looper.getMainLooper());
+
+        // Obteniendo los valores insertados
+        ExecutorService executorService = Executors.newSingleThreadExecutor();
+        executorService.execute(() -> {
+            List<Cliente> lstClientes = db.clienteDao().obtenerTodosLosClientes();
+            handler.post(() -> {
+                // Muestra el mensaje de que no hay clientes registrados segun sea el caso
+                if (!lstClientes.isEmpty()) {
+                    tvMensajeInfo.setVisibility(View.GONE);
+                } else {
+                    System.out.println("La lista esta vacia");
+                    tvMensajeInfo.setVisibility(View.VISIBLE);
+                }
+
+                ClienteAdaptador clienteAdaptador = new ClienteAdaptador(lstClientes, getContext(), this::actualizarListaClientes, activityLauncherClientes);
+                rcvClientes.setLayoutManager(new GridLayoutManager(getContext(), 1, GridLayoutManager.VERTICAL, false));
+                rcvClientes.setAdapter(clienteAdaptador);
+            });
+        });
     }
 }
